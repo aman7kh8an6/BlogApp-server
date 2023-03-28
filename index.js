d4024768af78849b3e5fe6885498572f064a7ea4
aman7kh8an6 <42239133+aman7kh8an6@users.noreply.github.com>
2023-03-28 15:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 const BASE_URL = process.env.BASE_URL
 
 var con = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "Aman@1998"
   });
